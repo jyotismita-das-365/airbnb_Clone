6418215e350b26d2f30848e0287cdd6485e0a935
jyotismita-das-365 <jyotismitadas365@gmail.com>
2026-01-23 14:32:56 +0530

    start creating airbnb server, host router user router 404 error handling

---
+++ airbnb2/app.js
@@ -0,0 +1,25 @@
+//Core Module
+const path = require('path');
+
+//External module
+const express = require('express');
+
+//Local module
+const userRouter = require("./routes/userRouter");
+const hostRouter = require("./routes/hostRouter");
+const rootDir = require('./utils/pathUtil');
+
+const app = express();
+
+app.use(express.urlencoded());
+app.use(userRouter);
+app.use("/host", hostRouter);
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', '404.html'));
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on address http://localhost:${PORT}`);
+});
